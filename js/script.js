a7024b2dee13437ed292379d64d06c6b3be154d6
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2021-06-26 14:21:51 +0200

    initial js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+{
+    const tasks = [
+        {
+            content: "wynieść śmieci",
+            done: false,
+           
+        },
+        {
+            content: "pobiegać",
+            done: true,
+            
+        },
+    ];
+
+
+        const render = () => {
+            let htmlString= ""
+            for(const task of tasks) {
+                htmlString += `
+                <li>
+                ${task.content}
+                </li>
+                `;
+            }
+
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        };
+    
+   
+    const init = () => {
+        render();
+
+
+    };
+
+    init();
+   
+}
