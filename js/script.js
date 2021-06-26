28bd7a32fb1766688bb19672cd59421d7638573d
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2021-06-26 15:10:14 +0200

    css for done tasks and grid for list items

--- js/script.js
+++ js/script.js
@@ -3,36 +3,43 @@
         {
             content: "wynieść śmieci",
             done: false,
-           
+
         },
         {
             content: "pobiegać",
             done: true,
-            
+
         },
     ];
 
 
-        const render = () => {
-            let htmlString= ""
-            for(const task of tasks) {
-                htmlString += `
-                <li>
+    const render = () => {
+        let htmlString = ""
+        for (const task of tasks) {
+            htmlString += `
+                <li class=" ${task.done ? "doneTask list__item" : "list__item"}">
+                <button></button>
                 ${task.content}
+                <button></button>
                 </li>
                 `;
-            }
+        }
 
-            document.querySelector(".js-tasks").innerHTML = htmlString;
-        };
-    
-   
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault();
+        });
 
 
     };
 
     init();
-   
+
 }
